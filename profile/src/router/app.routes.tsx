0f2d21db9779221d845b47bc47685503354b806b
Cleiton barros <ton.barros123@hotmail.com>
2023-09-22 17:48:04 -0300

    :sparkles: feat: update routes add contact, professional and services  path

--- profile/src/router/app.routes.tsx
+++ profile/src/router/app.routes.tsx
@@ -4,7 +4,13 @@ import { Container } from "../Components/Container";
 import { Footer } from "../Components/footer";
 import { Header } from "../Components/header";
 import { About } from "../page/About";
+import { Contact } from "../page/Contact";
+import { Services } from "../page/Expertise";
+import { Dietician } from "../page/Expertise/Dietician";
+import { Physio } from "../page/Expertise/physio";
+import { Psique } from "../page/Expertise/Psique";
 import { Home } from "../page/Home";
+import { Professional } from "../page/Professionals";
 
 export function AppRoutes() {
   return (
@@ -14,6 +20,12 @@ export function AppRoutes() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/professionals" element={<Professional />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services/nutricionista" element={<Dietician />} />
+          <Route path="/services/fisioterapia" element={<Physio />} />
+          <Route path="/services/menteepsique" element={<Psique />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Container>
       <Footer />
